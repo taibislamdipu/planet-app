185c04df284c4979e2d8ab71c7b6c5b8989d6fc2
taibislamdipu <mailtaibislam@gmail.com>
2022-05-22 03:26:41 +0600

    new

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 import React from 'react'
 import { colors } from './src/theme/colors'
 import { spacing } from './src/theme/spacing'
+import { typography } from './src/theme/typography'
+import Text from './src/components/text/text'
+import { useFonts } from 'expo-font';
 
 export default function App() {
+
+  const [loaded] = useFonts({
+    'Antonio-Medium': require('./assets/fonts/Antonio-Medium.ttf'),
+    'LeagueSpartan-Bold': require('./assets/fonts/LeagueSpartan-Bold.ttf'),
+    'LeagueSpartan-Regular': require('./assets/fonts/LeagueSpartan-Regular.ttf'),
+  });
+
+  if (!loaded) {
+    return <Text>Font is loading...</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>App</Text>
-      <Text style={{ marginTop: spacing[4] }}>Hello, world!</Text>
+      <Text preset="h1" style={{ color: "blue" }}>lorem ipsum dolor sit amet, con</Text>
+      <Text preset="small" style={{ marginTop: spacing[4] }}>Hello, world!</Text>
     </View>
   )
 }
@@ -15,7 +29,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: colors.darkOrange,
+    backgroundColor: "black",
     alignItems: 'center',
     justifyContent: 'center',
   }
