6926fdc7ff6b07ac7453291d3c2c26bd6193d5da
taibislamdipu <mailtaibislam@gmail.com>
2022-05-22 05:20:19 +0600

    new

---
+++ src/data/planet-list.js
@@ -0,0 +1,47 @@
+export const PLANET_LIST = [
+  {
+    name: "Mercury",
+    moons: [],
+    color: "#eefffd",
+  },
+  {
+    name: "Venus",
+    moons: [],
+    color: "#22dddd",
+  },
+  {
+    name: "Earth",
+    moons: [],
+    color: "#A865C9",
+  },
+  {
+    name: "Mars",
+    moons: [],
+    color: "green",
+  },
+  {
+    name: "Ceres",
+    moons: [],
+    color: "yellow",
+  },
+  {
+    name: "Jupiter",
+    moons: [],
+    color: "red",
+  },
+  {
+    name: "Saturn",
+    moons: [],
+    color: "olive",
+  },
+  {
+    name: "Uranus",
+    moons: [],
+    color: "skyblue",
+  },
+  {
+    name: "Neptune",
+    moons: [],
+    color: "orange",
+  },
+];
--- src/screens/home.js
+++ src/screens/home.js
@@ -1,14 +1,34 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, FlatList } from "react-native";
 import React from "react";
 import Text from "../components/text/text";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import PlanetHeader from "../components/planet-header";
 import { colors } from "../theme/colors";
+import { PLANET_LIST } from "../data/planet-list";
+import { keyExtractor } from "react-native/Libraries/Lists/VirtualizeUtils";
+import { spacing } from "../theme/spacing";
 
 export default function Home() {
   return (
     <SafeAreaView style={styles.container}>
-      <PlanetHeader></PlanetHeader>
+      <PlanetHeader />
+      <FlatList
+        contentContainerStyle={styles.list}
+        data={PLANET_LIST}
+        keyExtractor={(item) => item.name}
+        renderItem={({ item }) => {
+          const { name, color } = item;
+          return (
+            <View style={styles.item}>
+              <View style={[styles.circle, { backgroundColor: color }]} />
+              <Text preset="h4" style={styles.itemName}>
+                {name}
+              </Text>
+            </View>
+          );
+        }}
+        ItemSeparatorComponent={() => <View style={styles.separator} />}
+      />
     </SafeAreaView>
   );
 }
@@ -17,5 +37,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.black,
+  },
+  item: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: spacing[4],
+  },
+  itemName: {
+    textTransform: "uppercase",
+    marginLeft: spacing[4],
+  },
+  circle: {
+    width: 30,
+    height: 30,
+    borderRadius: 20,
+  },
+  list: {
+    padding: spacing[5],
+  },
+  separator: {
+    borderBottomColor: colors.white,
+    borderBottomWidth: 0.5,
   },
 });
