37a76f3463c877a6c23ca7787f66c45d748d5891
taibislamdipu <mailtaibislam@gmail.com>
2022-05-24 19:00:48 +0600

    new

--- src/data/planet-list.js
+++ src/data/planet-list.js
@@ -1,50 +1,32 @@
 export const PLANET_LIST = [
   {
     name: "mercury",
-
     color: "#DEF4FC",
-
     description:
       "Mercury is the smallest planet in the Solar System and the closest to the Sun. Its orbit around the Sun takes 87.97 Earth days, the shortest of all the Sun's planets. Mercury is one of four terrestrial planets in the Solar System, and is a rocky body like Earth.",
-
     rotationTime: 58.6,
-
     revolutionTime: "87.97 days",
-
     radius: 2439.7,
-
     avgTemp: "430°c",
-
     wikiLink: "https://en.wikipedia.org/wiki/Mercury",
-
     structure:
       "Mercury appears to have a solid silicate crust and mantle overlying a solid, iron sulfide outer core layer, a deeper liquid core layer, and a solid inner core. The planet's density is the second highest in the Solar System at 5.427 g/cm3 , only slightly less than Earth's density.",
-
     surface:
       "Mercury's surface is similar in appearance to that of the Moon, showing extensive mare-like plains and heavy cratering, indicating that it has been geologically inactive for billions of years. It is more heterogeneous than either Mars's or the Moon’s.",
   },
 
   {
     name: "venus",
-
     color: "#F7CC7F",
-
     description:
       "Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty. As the brightest natural object in Earth's night sky after the Moon, Venus can cast shadows and can be, on rare occasions, visible to the naked eye in broad daylight.",
-
     rotationTime: 243,
-
     revolutionTime: "224.7 days",
-
     radius: 6051.8,
-
     avgTemp: "471°c",
-
     wikiLink: "https://en.wikipedia.org/wiki/Venus",
-
     structure:
       "The similarity in size and density between Venus and Earth suggests they share a similar internal structure: a core, mantle, and crust. Like that of Earth, Venusian core is most likely at least partially liquid because the two planets have been cooling at about the same rate.",
-
     surface:
       "Much of the Venusian surface appears to have been shaped by volcanic activity. Venus has several times as many volcanoes as Earth, and it has 167 large volcanoes that are over 100 km (60 mi) across. The only volcanic complex of this size on Earth is the Big Island of Hawaii.",
   },
--- src/screens/details.js
+++ src/screens/details.js
@@ -1,15 +1,83 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, View, ScrollView, Image } from "react-native";
 import React from "react";
 import Text from "../components/text/text";
 import { SafeAreaView } from "react-native-safe-area-context";
 import PlanetHeader from "../components/planet-header";
 import { colors } from "../theme/colors";
+import { spacing } from "../theme/spacing";
 
-export default function Details({ navigation }) {
+export default function Details({ navigation, route }) {
+  const planet = route.params.planet;
+  const { name } = planet;
+  console.log("planet", planet);
+
+  const renderImage = (name) => {
+    switch (name) {
+      case "mercury":
+        return (
+          <Image
+            style={styles.imageSize}
+            source={require("../images/mercury.png")}
+          />
+        );
+      case "venus":
+        return (
+          <Image
+            style={styles.imageSize}
+            source={require("../images/venus.png")}
+          />
+        );
+      case "earth":
+        return (
+          <Image
+            style={styles.imageSize}
+            source={require("../images/earth.png")}
+          />
+        );
+      case "mars":
+        return (
+          <Image
+            style={styles.imageSize}
+            source={require("../images/venus.png")}
+          />
+        );
+      case "jupiter":
+        return (
+          <Image
+            style={styles.imageSize}
+            source={require("../images/venus.png")}
+          />
+        );
+      case "saturn":
+        return (
+          <Image
+            style={styles.imageSize}
+            source={require("../images/venus.png")}
+          />
+        );
+      case "uranus":
+        return (
+          <Image
+            style={styles.imageSize}
+            source={require("../images/venus.png")}
+          />
+        );
+      case "neptune":
+        return (
+          <Image
+            style={styles.imageSize}
+            source={require("../images/venus.png")}
+          />
+        );
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <PlanetHeader backBtn={true} />
-      <Text>Details</Text>
+      <ScrollView>
+        <View style={styles.imageView}>{renderImage(name)}</View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -18,5 +86,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.black,
+  },
+  imageView: {
+    padding: spacing[5],
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  imageSize: {
+    width: 400,
+    height: 400,
   },
 });
